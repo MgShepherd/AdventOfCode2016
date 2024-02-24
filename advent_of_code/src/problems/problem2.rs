4db7523a729faf1c4c315a9fb95774b302d06c18
MgShepherd <michaelshep1504@gmail.com>
2024-02-24 17:11:48 +0000

    Solved Problem 2 Part 2

--- advent_of_code/src/problems/problem2.rs
+++ advent_of_code/src/problems/problem2.rs
@@ -1,7 +1,7 @@
-use crate::utils::data_utils::Point;
+use crate::utils::data_utils::IndexPoint;
 
 pub fn solve(data: &str) {
-    let mut current_point = Point { x: 1, y: 1 };
+    let mut current_point = IndexPoint { x: 0, y: 2 };
     let mut code = String::new();
 
     for line in data.split('\n').filter(|x| !x.is_empty()) {
@@ -10,21 +10,38 @@ pub fn solve(data: &str) {
     println!("Code is: {code}");
 }
 
-fn get_number(current_point: &mut Point, line: &str) -> char {
-    const KEYPAD_SIZE: i32 = 3;
-    const KEYPAD: [[char; KEYPAD_SIZE as usize]; KEYPAD_SIZE as usize] =
-        [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']];
+fn get_number(current_point: &mut IndexPoint, line: &str) -> char {
+    const KEYPAD_SIZE: usize = 5;
+    const KEYPAD: [[char; KEYPAD_SIZE]; KEYPAD_SIZE] = [
+        [' ', ' ', '1', ' ', ' '],
+        [' ', '2', '3', '4', ' '],
+        ['5', '6', '7', '8', '9'],
+        [' ', 'A', 'B', 'C', ' '],
+        [' ', ' ', 'D', ' ', ' '],
+    ];
 
     for element in line.chars() {
-        if element == 'U' && current_point.y > 0 {
+        if element == 'U'
+            && current_point.y > 0
+            && KEYPAD[current_point.y - 1][current_point.x] != ' '
+        {
             current_point.y -= 1;
-        } else if element == 'D' && current_point.y < KEYPAD_SIZE - 1 {
+        } else if element == 'D'
+            && current_point.y < KEYPAD_SIZE - 1
+            && KEYPAD[current_point.y + 1][current_point.x] != ' '
+        {
             current_point.y += 1;
-        } else if element == 'L' && current_point.x > 0 {
+        } else if element == 'L'
+            && current_point.x > 0
+            && KEYPAD[current_point.y][current_point.x - 1] != ' '
+        {
             current_point.x -= 1;
-        } else if element == 'R' && current_point.x < KEYPAD_SIZE - 1 {
+        } else if element == 'R'
+            && current_point.x < KEYPAD_SIZE - 1
+            && KEYPAD[current_point.y][current_point.x + 1] != ' '
+        {
             current_point.x += 1;
         }
     }
-    KEYPAD[current_point.y as usize][current_point.x as usize]
+    KEYPAD[current_point.y][current_point.x]
 }
--- advent_of_code/src/utils/data_utils.rs
+++ advent_of_code/src/utils/data_utils.rs
@@ -3,3 +3,9 @@ pub struct Point {
     pub x: i32,
     pub y: i32,
 }
+
+#[derive(Debug, Hash, Eq, PartialEq, Clone, Copy)]
+pub struct IndexPoint {
+    pub x: usize,
+    pub y: usize,
+}
