009dab168dbf2bcfc8072a2bae368271478450af
MgShepherd <michaelshep1504@gmail.com>
2024-02-26 21:07:53 +0000

    Solved Problem 3 Part 2

--- advent_of_code/src/problems/problem3.rs
+++ advent_of_code/src/problems/problem3.rs
@@ -1,22 +1,36 @@
+const TRIANGLE_SIZE: usize = 3;
+
 pub fn solve(data: &str) {
     let mut num_valid_triangles = 0;
-    for line in data.split('\n') {
-        if is_triangle_valid(line) {
-            num_valid_triangles += 1;
+    let mut data_iter = data.split('\n');
+    loop {
+        let lines = vec![data_iter.next(), data_iter.next(), data_iter.next()];
+
+        if lines.contains(&None) {
+            break;
         }
+        num_valid_triangles += get_valid_triangles(
+            lines
+                .iter()
+                .map(|x| convert_line_to_values(x.unwrap()))
+                .collect(),
+        );
     }
     println!("Num valid Triangles: {num_valid_triangles}");
 }
 
-fn is_triangle_valid(triangle: &str) -> bool {
+fn get_valid_triangles(elements: Vec<Vec<i32>>) -> i32 {
+    let mut sum = 0;
+    for i in 0..TRIANGLE_SIZE {
+        sum += is_triangle_valid([elements[0][i], elements[1][i], elements[2][i]]) as i32
+    }
+    sum
+}
+
+fn is_triangle_valid(elements: [i32; TRIANGLE_SIZE]) -> bool {
     let mut min_sum = 0;
     let mut max_value = 0;
-    for element in triangle
-        .split(' ')
-        .map(|x| x.trim())
-        .filter(|x| x.len() > 0)
-        .map(|x| x.parse::<i32>().unwrap())
-    {
+    for element in elements {
         if element <= max_value {
             min_sum += element;
         } else {
@@ -25,4 +39,12 @@ fn is_triangle_valid(triangle: &str) -> bool {
         }
     }
     min_sum > max_value
+}
+
+fn convert_line_to_values(line: &str) -> Vec<i32> {
+    line.split(' ')
+        .map(|x| x.trim())
+        .filter(|x| x.len() > 0)
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect()
 }
