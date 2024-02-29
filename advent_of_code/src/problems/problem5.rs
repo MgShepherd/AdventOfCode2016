f74490b8060b7d5e97759263a0d0090cb5baf4ce
MgShepherd <michaelshep1504@gmail.com>
2024-02-29 20:27:42 +0000

    Solved Problem 5 Part 2

--- advent_of_code/src/problems/problem5.rs
+++ advent_of_code/src/problems/problem5.rs
@@ -1,18 +1,32 @@
 const PUZZLE_INPUT: &str = "ugkcyxxp";
+const PASSWORD_LENGTH: usize = 8;
 const NUM_LEADING_ZEROS: usize = 5;
 
 pub fn solve() {
     let mut index = 0;
-    let mut password = String::new();
+    let mut password_array = ['_'; PASSWORD_LENGTH];
     loop {
-        if password.len() == 8 {
+        if !password_array.contains(&'_') {
             break;
         }
         let hash = format!("{:?}", md5::compute(format!("{PUZZLE_INPUT}{index}")));
         if &hash[0..NUM_LEADING_ZEROS] == "00000" {
-            password.push(hash.chars().nth(NUM_LEADING_ZEROS).unwrap());
+            let insert_index = hash
+                .chars()
+                .nth(NUM_LEADING_ZEROS)
+                .unwrap()
+                .to_digit(10)
+                .unwrap_or(u32::MAX) as usize;
+            if insert_index < PASSWORD_LENGTH && password_array[insert_index] == '_' {
+                println!("{hash}");
+                password_array[insert_index] = hash.chars().nth(NUM_LEADING_ZEROS + 1).unwrap();
+            }
         }
         index += 1;
     }
-    println!("The Door Password is: {password}");
+    println!();
+    for element in password_array {
+        print!("{element}");
+    }
+    println!();
 }
