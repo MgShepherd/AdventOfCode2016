98487bc4140c6f7d68a308f83cc3996dc2bff1c7
MgShepherd <michaelshep1504@gmail.com>
2024-03-01 22:37:03 +0000

    Solved Problem 7 Part 2

--- advent_of_code/src/problems/problem7.rs
+++ advent_of_code/src/problems/problem7.rs
@@ -1,34 +1,46 @@
 pub fn solve(data: &str) {
-    let mut num_support_tls = 0;
+    let mut num_support_ssl = 0;
     for line in data.split('\n') {
-        num_support_tls += does_ip_support_tls(line) as u32;
+        num_support_ssl += does_ip_support_ssl(line) as u32;
     }
-    println!("Num IPs supporting TLS: {num_support_tls}");
+    println!("Num IPs supporting SSL: {num_support_ssl}");
 }
 
-fn does_ip_support_tls(ip_str: &str) -> bool {
+fn does_ip_support_ssl(ip_str: &str) -> bool {
     let mut valid = false;
-    for (index, section) in ip_str.split(['[', ']']).filter(|x| x.len() > 0).enumerate() {
-        if !valid || index % 2 == 1 {
-            let contains_abba = does_section_contain_abba(section);
-            if contains_abba && index % 2 == 1 {
-                valid = false;
-                break;
-            } else if contains_abba {
-                valid = true;
-            }
+    let sections: Vec<&str> = ip_str.split(['[', ']']).filter(|x| x.len() > 0).collect();
+    let mut bab_sequences: Vec<String> = Vec::new();
+    for i in (0..sections.len()).step_by(2) {
+        bab_sequences.append(&mut get_bab_sequences(sections[i]));
+    }
+    for i in (1..sections.len()).step_by(2) {
+        if does_contain_sequence(sections[i], &bab_sequences) {
+            valid = true;
+            break;
         }
     }
     valid
 }
 
-fn does_section_contain_abba(section: &str) -> bool {
+fn get_bab_sequences(section: &str) -> Vec<String> {
+    let mut sequences: Vec<String> = Vec::new();
     let section_chars: Vec<char> = section.chars().collect();
-    for i in 0..section_chars.len() - 3 {
-        if section_chars[i] == section_chars[i + 3]
-            && section_chars[i + 1] == section_chars[i + 2]
-            && section_chars[i] != section_chars[i + 1]
-        {
+    for i in 0..section_chars.len() - 2 {
+        if section_chars[i] == section_chars[i + 2] && section_chars[i] != section_chars[i + 1] {
+            sequences.push(format!(
+                "{}{}{}",
+                section_chars[i + 1],
+                section_chars[i],
+                section_chars[i + 1]
+            ));
+        }
+    }
+    sequences
+}
+
+fn does_contain_sequence(section: &str, sequences: &Vec<String>) -> bool {
+    for sequence in sequences {
+        if section.contains(sequence) {
             return true;
         }
     }
