9ead67111e6680de53cb21e2bcc6e02a33158e1f
MgShepherd <michaelshep1504@gmail.com>
2024-03-01 14:35:27 +0000

    Solved Problem 6 Part 1

--- advent_of_code/src/main.rs
+++ advent_of_code/src/main.rs
@@ -3,9 +3,9 @@ use advent_of_code::utils::file_utils;
 use std::process;
 
 fn main() {
-    let problem_data = file_utils::read_problem_file_contents(4).unwrap_or_else(|err| {
+    let problem_data = file_utils::read_problem_file_contents(6).unwrap_or_else(|err| {
         println!("Problem loading data: {err}");
         process::exit(1);
     });
-    problems::problem5::solve();
+    problems::problem6::solve(&problem_data);
 }
---
+++ advent_of_code/src/problems/problem6.rs
@@ -0,0 +1,25 @@
+use std::collections::HashMap;
+
+const MESSAGE_LENGTH: usize = 8;
+
+pub fn solve(data: &str) {
+    let mut letter_frequencies: Vec<HashMap<char, usize>> = vec![HashMap::new(); MESSAGE_LENGTH];
+    for line in data.split('\n') {
+        for (pos, element) in line.chars().enumerate() {
+            let current_element = letter_frequencies[pos].entry(element).or_insert(0);
+            *current_element += 1;
+        }
+    }
+
+    let mut decoded_message = String::new();
+    for frequency_map in letter_frequencies {
+        decoded_message.push(
+            *frequency_map
+                .iter()
+                .max_by(|a, b| a.1.cmp(&b.1))
+                .map(|(k, _v)| k)
+                .unwrap(),
+        )
+    }
+    println!("The decoded message is: {decoded_message}");
+}
